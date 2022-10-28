88eefc206847cabbeb91633b4fbf861f385b9fa5
afimbres <afimbres@nmetric.com>
2022-10-27 17:13:03 -0700

    About is done

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Who Am I?</h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+    </section>
+  );
+}
+
+export default About;
