2fd96896e5fefa5d7e7a4dd117f1f1adee2de146
afimbres <afimbres@nmetric.com>
2022-10-28 23:02:11 -0700

    Gallery wrapped up

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         currentCategory={currentCategory}
       />
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About />
       </main>
     </div>
---
+++ src/components/Gallery/PhotoList/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import PhotoList from "../";
+
+afterEach(cleanup);
+
+describe("PhotoList is rendering", () => {
+  it("renders", () => {
+    render(<PhotoList />);
+  });
+
+  it("renders to snapshot", () => {
+    const { asFragment } = render(<PhotoList />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders from photo list depending on category passed", () => {
+    const { asFragment } = render(<PhotoList category={"portraits"} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
